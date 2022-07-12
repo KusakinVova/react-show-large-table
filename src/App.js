b6b720ef481317863c1352179df90dd57e7b7481
Vladimir <kusakinvova@gmail.com>
2022-07-12 16:27:25 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
+import React from 'react';
 import './App.css';
 
-function App() {
+function App({ data, rowHeight, visibleRows }) {
+  const rootRef = React.useRef();
+  const [start, setStart] = React.useState(0);
+  
+  function getTopHeight() {
+    return rowHeight * start;
+  }
+  function getBottomHeight() {
+    return rowHeight * (data.length - (start + visibleRows + 1));
+  }
+  
+  React.useEffect(() => {
+    function onScroll(e) {
+      setStart(Math.min(
+        data.length - visibleRows - 1,
+        Math.floor(e.target.scrollTop / rowHeight)
+      ));
+    }
+    rootRef.current.addEventListener('scroll', onScroll);
+
+    return () => {
+      rootRef.current.removeEventListener('scroll', onScroll);
+    }
+  }, [data.length, visibleRows, rowHeight]);
+  
   return (
-    <div className="App">
+    <div style={{ height: rowHeight * visibleRows + 1, overflow: 'auto' }} ref={rootRef}>
+      <div style={{ height: getTopHeight() }} />
+      <table>
+        <tbody>
+          {data.slice(start, start + visibleRows + 1).map((row, rowIndex) => (
+            <tr
+              style={{ height: rowHeight }}
+              key={start + rowIndex}
+            >{row.map((text, colIndex) => (
+              <td key={start + '' + rowIndex + colIndex}>{text}</td>
+            ))}</tr>
+          ))}
+        </tbody>
+      </table>
+      <div style={{ height: getBottomHeight() }} />  
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function makeTableData(w, h) {
+  return new Array(h).fill(0).map((_, row) => {
+    return new Array(w).fill(0).map((_, col) => {
+      return row * 10 + col;
+    });
+  });
+}
+
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      data={makeTableData(10, 10000)}
+      rowHeight={40}
+      visibleRows={10}
+    />
   </React.StrictMode>
 );
-
-
